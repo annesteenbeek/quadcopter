9953fed0845adf03c7b3ea36c77c2f321e392dcb
annesteenbeek <annesteenbeek@gmail.com>
2015-11-17 17:10:03 +0100

    working csv save

--- debugNode/public/script.js
+++ debugNode/public/script.js
@@ -1,7 +1,6 @@
 var app = angular.module('MyApp', []);
-google.load('visualization', '1.0', {'packages':['corechart']});
-dataTables = {};
-
+// ----------- Downlaod CSV ------------
+// --------------- socketio in angularjs ----------
 
 app.factory('socket', function ($rootScope) {
   var socket = io.connect();
@@ -30,6 +29,7 @@ app.factory('socket', function ($rootScope) {
 var serialPortList = [];
 
 app.controller('nodeSerial', function($scope, socket){
+  $scope.dataTables = {};
   $scope.connected = false;
   $scope.ports = ["..."];
 
@@ -54,38 +54,38 @@ app.controller('nodeSerial', function($scope, socket){
 
 // ------------ Inputs ----------------
 
-var chart = new google.visualization.LineChart(document.getElementById('myChart'));
-// var timeFormat = new google.visualization.DateFormat({formatType: 'msSS'});
-  var options = {
-    hAxis: {
-      title: 'Time'
-      // format: "S";
-    },
-    vAxis: {
-      title: 'Popularity'
-    }
-  };
-
   socket.on("serialData", function (input){
     console.log(input[0] +": " +input[1]);
-    if(input[0]=="aRoll"){
-      // timeStamp =new Date().getTime();
-
-      if(!dataTables[input[0]]){ // if new key value, creat new data table
-        dataTables[input[0]] = new google.visualization.DataTable();
-        dataTables[input[0]].addColumn('string', "timestamp");
-        dataTables[input[0]].addColumn('number', input[0]);
-        dataTables[input[0]].addRow([String(new Date('m:s:SS').getTime()), parseFloat(input[1])]);
+      if(!$scope.dataTables[input[0]]){ // if new key value, creat new data table
+        $scope.dataTables[input[0]] = [input[1]];
       }else{
-        dataTables[input[0]].addRow([String(new Date('m:s:SS').getTime()), parseFloat(input[1])]);
-        chart.draw(dataTables["aRoll"], options);
+        $scope.dataTables[input[0]].push(input[1]);
       };
-      console.log(dataTables);
-    }
-
   });
 
+// --------------- Download csv ------------
+$scope.getCSV = function (name){
+  var csvContent = "data:text/csv;charset=utf-8,";
+  data = [$scope.dataTables[name]];
+       data.forEach(function(infoArray, index){ 
+          dataString = infoArray.join(","); 
+          csvContent += index < infoArray.length ? dataString+ "\n" : dataString;  
+        });
+       console.log(dataString);
+    var encodedUri = encodeURI(csvContent);
+    // window.open(encodedUri);
+    // dirty method to set download name
+    var link = document.createElement("a");    
+    link.href = encodedUri;
 
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = name + ".csv";
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
 //-------------- Plotting --------------
 
 // ------------ Serial port setup ------
@@ -96,16 +96,11 @@ var chart = new google.visualization.LineChart(document.getElementById('myChart'
   $scope.connectSerial = function(){
     console.log("Connecting to: "+$scope.selectedPort);
     socket.emit('connectSerial', $scope.selectedPort)
-  }
+  };
   $scope.disconnectSerial = function(){
     console.log("Disconnecting serialport");
     socket.emit('disconnectSerial');
-  }
-
-// ----------- Button functions --------
-  $scope.storeData = function(){
-    console.log("storing the data");
-    socket.emit("storeData");
-  }
-
+  };
 })
+
+
